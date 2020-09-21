a6d0a88f6181c24ffe00b1b1f132a5bde8b507c8
eratcliff <eratcliffhr@gmail.com>
2020-09-20 20:51:20 -0700

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// console.log("hello");
+
+// // inside db/index.js
+// const { Client } = require('pg'); // imports the pg module
+
+// // supply the db name and location of the database
+// const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+// module.exports = {
+//   client,
+// }
+
